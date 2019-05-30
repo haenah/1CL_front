5425e555f2de8e4d23c73ecd1b93a3a5626c9fb0
BaekGeunYoung <dvmflstm@naver.com>
2019-05-30 13:36:06 +0900

    css fix

--- onecl-front/src/components/ClubDetail/Header/Header.js
+++ onecl-front/src/components/ClubDetail/Header/Header.js
@@ -4,9 +4,9 @@ import './Header.css'
 export const Header = ({id, clickInfoPostButton, clickMemberListButton, clickDocListButton}) => {
     return(
         <div className={'wrapper'}>
-            <button className='options' onClick={() => clickInfoPostButton()}> recruit </button>
-            <button className='options' onClick={() => clickDocListButton()}> board </button>
-            <button className='options' onClick={() => clickMemberListButton()}> member list </button>
+            <button className='headerOptions' onClick={() => clickInfoPostButton()}> recruit </button>
+            <button className='headerOptions' onClick={() => clickDocListButton()}> board </button>
+            <button className='headerOptions' onClick={() => clickMemberListButton()}> member list </button>
         </div>
     )
 };
