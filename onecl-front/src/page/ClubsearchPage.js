439d68b35d243ebeff70a80c1c77b59a3e38288b
mymoto <mymoto23@naver.com>
2019-06-15 20:25:24 +0900

    fixed problem where page doesnt response

--- onecl-front/src/page/ClubsearchPage.js
+++ onecl-front/src/page/ClubsearchPage.js
@@ -4,13 +4,7 @@ import ClubListContainer from '../containers/Clubsearch/ClubListContainer';
 
 const ClubsearchPage = () => {
     return(
-        <div style={{
-            'height' : '100%',
-            'width' : '100%',
-            'position' : 'fixed',
-            'z-index' : '-10',
-            'background' : 'linear-gradient(-135deg, #c0dcf3, white)',
-        }}>
+        <div>
             <CategoryContainer/>
             <ClubListContainer/>
         </div>
